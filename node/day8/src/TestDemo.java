ff9d0711f9bd13635695fa54966745485cc57f8d
wj1226 <74949102+wj1226@users.noreply.github.com>
2022-03-30 23:36:36 +0800

    day8
    day8

--- node/day8/src/TestDemo.java
+++ node/day8/src/TestDemo.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.Scanner;
 
 interface A {
-     public abstract void fun();
+    public abstract void fun();
 }
 
 interface B {}
@@ -12,12 +16,66 @@ class C {
 
 }
 
+/*abstract class Test1 {
+    abstract void method();
+}
+
+abstract class Test2 {
+    abstract void method();
+}
+
+abstract class Test3 {
+    abstract void method();
+}
+
+class Test4 {
+    final void method() { }
+}*/
+
 public class TestDemo extends C implements A, B{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int m = a;
+        int n = b;
+        int c = 0;
+        while ((c = a % b) != 0) {
+            a = b;
+            b = c;
+        }
+        System.out.println(m*n/b);
+    }
+
+    public static void main7(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = Math.max(a, b);
+        while (true) {
+            if (c % a == 0 && c % b == 0) {
+                System.out.println(c);
+                break;
+            }
+            c++;
+        }
+    }
+
+    public static void mai6(String[] args) throws IOException {
+        // BufferedReader 从字符流中读取文本并且缓存
+        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bi.readLine());
+    }
+
     public static boolean chkParenthesis(String A, int n) {
         // write code here
+        if (n % 2 != 0) {
+            return false;
+        }
         Deque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            char ch = A.charAt(i);
+        /* for (int i = 0; i < n; i++) {
+            char ch = A.charAt(i); */
+        for (char ch : A.toCharArray()) {
             if (ch == '(') { // 左括号入栈
                 stack.offer(ch);
             } else {
@@ -31,13 +89,21 @@ public class TestDemo extends C implements A, B{
                 }
             }
         }
-        if (stack.isEmpty()) {
-            return true;
-        }
-        return false;
+        return stack.isEmpty();
     }
 
-    public static void main(String[] args) {
+    public static void main4(String[] args) {
+        String s1 = "abcd";
+        String s2 = s1.toLowerCase(); // 与 "abcd" 一样 返回原来的对象
+        System.out.println(s1 == s2); // true
+        System.out.println(s1.toLowerCase() == "abcd"); // true
+
+        String s3 = "Admin" ;
+        String s4 = s3.toLowerCase(); // 大写转小写 不一样 产生了一个新的对象
+        System.out.println(s3 == "admin"); // false
+    }
+
+    public static void main3(String[] args) {
         Boolean flg = chkParenthesis("()()(((())))", 12);
         System.out.println(flg);
     }
