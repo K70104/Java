685277c400b0c22d2f6bb9a28d43fb0bf905971f
wj1226 <74949102+wj1226@users.noreply.github.com>
2022-04-14 00:32:46 +0800

    day15
    day15

---
+++ node/day15/src/TestDemo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (perfectNum(i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static Boolean perfectNum(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return n == sum;
+    }
+
+    public static void main1(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("1");
+            }
+        });
+        t.start();
+        System.out.println("2");
+    }
+}
